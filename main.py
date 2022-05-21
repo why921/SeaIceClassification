bfa85a3d804903cd749cb46fdcbade243d73ba3c
why <359226737@qq.com>
2022-05-21 13:34:22 +0800

    temp0521

--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# 这是一个示例 Python 脚本。
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torch import nn, optim
+from spectrogramDataset import spectrogramDataset
+from pauliDataset import pauliDataset
+from net_test import CNeuralNetwork
+from osgeo import gdal
+from pauliDataset import data_transforms
+import numpy as np
+import os
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+def main():
+
+    batchsz=100
+    in_ds = SeaIceDataset(labeltxt='D:\why2022\seaice\data_process\green.txt', transform=data_transforms)
+    in_ds.__init__(labeltxt='D:\why2022\seaice\data_process\green.txt', transform=data_transforms)
+
+    ev_ds=evlDataset(labeltxt='D:\why2022\seaice\mi3w0409\ALOS-P1_1__A-ORBIT__ALPSRP256411570_Cal_ML_Spk_Decomppauli.txt', transform=data_transforms)
+    ev_ds.__init__(labeltxt='D:\why2022\seaice\mi3w0409\ALOS-P1_1__A-ORBIT__ALPSRP256411570_Cal_ML_Spk_Decomppauli.txt', transform=data_transforms)
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+    train_loader = torch.utils.data.DataLoader(dataset=in_ds,
+                                               batch_size=batchsz,
+                                               shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=ev_ds,
+                                              batch_size=batchsz,
+                                              shuffle=False)
+    device = torch.device('cuda')
 
+    x, label = iter(train_loader).next()
+    print('x:', x.shape, 'label:', label.shape)
 
-# 按间距中的绿色按钮以运行脚本。
+    model = CNeuralNetwork().to(device)
+
+   # loss_func = nn.MSELoss()
+   #criteon = nn.CrossEntropyLoss().to(device)
+    loss_func=nn.CrossEntropyLoss().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    print(model)
+
+    idtxt = open('test.txt', 'w')
+    for epoch in range(1000):
+        model.train()
+        for batchidx, (x, label) in enumerate(train_loader):
+            x, label = x.to(device), label.to(device)
+            #print(x.shape)
+            logits = model(x)
+            loss = loss_func(logits, label - 1)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+# print(epoch, 'loss:', loss.item())
+    model.eval()
+    with torch.no_grad(),open('test.txt','a') as f:
+        for x, label in test_loader:
+        # [b, 3, 32, 32]
+        # [b]
+          x, label = x.to(device), label.to(device)
+          logits = model(x)
+          pred = logits.argmax(dim=1)
+          #pprreedd=pred.cpu().numpy()
+          pprreedd = np.array(pred.cpu())
+          #print(pprreedd)
+          np.savetxt(f, pprreedd, fmt='%d',delimiter=' ')
+          #idtxt.write(pprreedd)
+    idtxt.close()
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
